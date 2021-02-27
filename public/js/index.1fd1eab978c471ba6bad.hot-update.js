55ed936449d5c26ef79c37e6b94c7ef33fde4ed0
cijin <cijininurhood@gmail.com>
2021-02-27 11:37:12 +0700

    after almost a week, the dev env is set up. Phew (sweat_smiley) :D

---
+++ public/js/index.1fd1eab978c471ba6bad.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatenodecodives"]("/js/index",{
+
+/***/ "./resources/assets/scss/index.scss":
+/*!******************************************!*\
+  !*** ./resources/assets/scss/index.scss ***!
+  \******************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n    if(true) {\n      // 1614400583749\n      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ \"./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js\")(module.id, {\"esModule\":true,\"locals\":false});\n      module.hot.dispose(cssReload);\n      module.hot.accept(undefined, cssReload);\n    }\n  //# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ub2RlY29kaXZlcy8uL3Jlc291cmNlcy9hc3NldHMvc2Nzcy9pbmRleC5zY3NzPzU5MjciXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ1U7QUFDVixPQUFPLElBQVU7QUFDakI7QUFDQSxzQkFBc0IsbUJBQU8sQ0FBQyx3SkFBd0csY0FBYywrQkFBK0I7QUFDbkwsTUFBTSxVQUFVO0FBQ2hCLE1BQU0saUJBQWlCO0FBQ3ZCIiwiZmlsZSI6Ii4vcmVzb3VyY2VzL2Fzc2V0cy9zY3NzL2luZGV4LnNjc3MuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTtcbiAgICBpZihtb2R1bGUuaG90KSB7XG4gICAgICAvLyAxNjE0NDAwNTgzNzQ5XG4gICAgICB2YXIgY3NzUmVsb2FkID0gcmVxdWlyZShcIi9ob21lL2NpamluL1Byb2plY3RzL25vZGVjb2RpdmVzL25vZGVfbW9kdWxlcy9taW5pLWNzcy1leHRyYWN0LXBsdWdpbi9kaXN0L2htci9ob3RNb2R1bGVSZXBsYWNlbWVudC5qc1wiKShtb2R1bGUuaWQsIHtcImVzTW9kdWxlXCI6dHJ1ZSxcImxvY2Fsc1wiOmZhbHNlfSk7XG4gICAgICBtb2R1bGUuaG90LmRpc3Bvc2UoY3NzUmVsb2FkKTtcbiAgICAgIG1vZHVsZS5ob3QuYWNjZXB0KHVuZGVmaW5lZCwgY3NzUmVsb2FkKTtcbiAgICB9XG4gICJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./resources/assets/scss/index.scss\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => ("cb893a03852f054cd977")
+/******/ 	})();
+/******/ 	
+/******/ }
+);
---
+++ webpack.mix.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const mix = require('laravel-mix')
+
+require('laravel-mix-tailwind')
+
+// NOTE: Don't remove this, Because it's the default public folder path on AdonisJs
+mix.setPublicPath('public')
+
+// Add your assets here
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+mix
+  .setPublicPath('public')
+  .ts('resources/client/index.tsx', 'public/js/')
+  .react()
+  .sass('resources/assets/scss/index.scss', 'public/css')
+  .tailwind()
+  .options({
+    processCssUrls: false,
+  })
+
+if (isDevelopment) {
+  mix.sourceMaps()
+}
+
+mix.webpackConfig({
+  mode: isDevelopment ? 'development' : 'production',
+  context: __dirname,
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [isDevelopment && new webpack.HotModuleReplacementPlugin()].filter(
+    Boolean
+  ),
+})
