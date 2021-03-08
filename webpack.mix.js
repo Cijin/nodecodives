d1739e1aaf89749e58058ac9cbf35751b94c03e7
cijin <cijininurhood@gmail.com>
2021-03-08 08:09:41 +0700

    update webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const webpack = require('webpack')
 const Dotenv = require('dotenv-webpack')
 const mix = require('laravel-mix')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const outputPath = path.resolve(__dirname, 'public')
 
 require('laravel-mix-tailwind')
 
@@ -12,7 +16,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 mix
   .setPublicPath('public')
-  .ts('resources/client/index.tsx', 'public/js/')
+  .js('resources/client/index.tsx', 'public/js/')
   .react()
   .sass('resources/assets/scss/index.scss', 'public/css')
   .tailwind()
@@ -28,10 +32,6 @@ mix.webpackConfig({
   mode: isDevelopment ? 'development' : 'production',
   context: __dirname,
   target: 'node',
-  output: {
-    hotUpdateChunkFilename: 'hot/hot-update.js',
-    hotUpdateMainFilename: 'hot/hot-update.json',
-  },
   node: {
     __filename: true,
     __dirname: true,
@@ -50,6 +50,10 @@ mix.webpackConfig({
   },
   plugins: [
     isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [`${outputPath}/*.hot-update.*`],
+      dry: false,
+    }),
     new Dotenv(),
   ].filter(Boolean),
 })
